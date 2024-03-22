9a513dea54ad790cd0c056aa140956ab679e6232
Julio Monteiro <150204882+juliomonteiiro@users.noreply.github.com>
2024-03-22 20:45:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,16 @@
-#include <stdio.h>
 #include <locale.h>
+#include <stdio.h>
 
 int main(void) {
   setlocale(LC_ALL, "Portuguese");
-  
-  float 
-  nota1,
-  nota2,
-  media,
-  recuperacao;
-  
+
+  float nota1, nota2, media, recuperacao, media2;
+
   printf("Cálculo de média: \n \n");
-  
+
   printf("Digite a primeira nota: ");
   scanf("%f", &nota1);
-  
+
   printf("Digite a segunda nota: ");
   scanf("%f", &nota2);
 
@@ -22,28 +18,42 @@ int main(void) {
 
   if (media < 7) {
     printf("\t Sua media é %.2f \n", media);
-    
+
     printf("\t Você ficou de recuperação! \n");
-    
+
     printf("\t Digite a nota da prova de recuperação: \n");
     scanf("%f", &recuperacao);
+
+    if (nota1 > nota2) {
+      media2 = (nota1 + recuperacao) / 2;
+      
+      if (media2 < 7) {
+        printf("\t Você foi reprovado! \n");
+      } 
         
-    if (recuperacao < 7) 
-    {
-    
-      printf("\t Você foi reprovado! \n");
-         
+      else {
+        printf("\t Você foi aprovado na recuperação! Dedique-se mais aos estudos no próximo ano! \n");
+      }
     }
-    else 
+
+    else
     {
-          
-      printf("\t Você foi aprovado na recuperação!/nDedique-se mais ao estudos no próximo ano! \n");
-  
+      media2 = (nota2 + recuperacao) / 2;
+      
+      if (media2 < 7) {
+        printf("\t Você foi reprovado! \n");
+      } 
+      
+      else 
+      {
+        printf("\t Você foi aprovado na recuperação!/nDedique-se mais aos estudos no próximo ano! \n");
+      }
     }
   } 
-  else {
+  else
+  {
     printf("\t Parabéns você foi aprovado!");
   }
-  
+
   return 0;
 }
